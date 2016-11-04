fd139c609dd3e04ce939e1e106418af9ac6b64e0
Piotr Siwinski <siwinski.piotr@outlook.com>
2016-11-04 08:43:24 +0100

    Add project files.

---
+++ PMS.Harrier/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PMS.Harrier.Startup))]
+namespace PMS.Harrier
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
