2bc4f45a2309acb81069f2e5156b04abed31f4df
Mateusz Byczkowski <byczkowski38@gmail.com>
2016-12-01 17:39:48 +0100

    zmiana stylów style.css

--- PMS.Harrier.WebUI/App_Start/BundleConfig.cs
+++ PMS.Harrier.WebUI/App_Start/BundleConfig.cs
@@ -23,8 +23,9 @@ namespace PMS.Harrier.WebUI
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/site.css",
+                "~/Content/bootstrap.css"
+                      ));
         }
     }
 }
