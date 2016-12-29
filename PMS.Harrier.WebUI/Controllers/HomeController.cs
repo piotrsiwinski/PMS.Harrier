c47ae13f6a126130160ce540783dcb0537adc622
Piotrek <siwinski.piotr@outlook.com>
2016-12-29 17:12:28 +0100

    Changed main view

--- PMS.Harrier.WebUI/Controllers/HomeController.cs
+++ PMS.Harrier.WebUI/Controllers/HomeController.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using PMS.Harrier.BusinessLogicLayer.Abstract;
+using PMS.Harrier.DataAccessLayer.Models;
+using PMS.Harrier.WebUI.ViewModels;
 
 namespace PMS.Harrier.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProjectLogic _projectLogic;
+
+        public HomeController(IProjectLogic projectLogic)
+        {
+            AutoMapper.Mapper.CreateMap<Project, ProjectViewModel>();
+            _projectLogic = projectLogic;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -22,6 +34,16 @@ namespace PMS.Harrier.WebUI.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+        [ChildActionOnly]
+        public ActionResult MyProjects()
+        {
+            var loggedUser =
+                System.Web.HttpContext.Current.GetOwinContext()
+                    .GetUserManager<ApplicationUserManager>()
+                    .FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            var userProjects = _projectLogic.GetProjectsByDeveloperId(loggedUser.Developer.DeveloperId);
+            return PartialView("_myProjects",AutoMapper.Mapper.Map<List<Project>, List<ProjectViewModel>>(userProjects));
         }
     }
 }
